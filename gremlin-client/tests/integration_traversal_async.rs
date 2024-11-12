fb3bf91b2500f61fa61c7828eab94fd4a4e5449e
Allan Clements <criminosis@gmail.com>
2024-11-12 14:22:21 -0600

    Parameterized integration_traversal_async

--- gremlin-client/tests/integration_traversal_async.rs
+++ gremlin-client/tests/integration_traversal_async.rs
@@ -2,9 +2,14 @@ mod common;
 
 #[cfg(feature = "async_gremlin")]
 mod aio {
-    use gremlin_client::process::traversal::traversal;
+    use rstest::*;
+    use rstest_reuse::{self, *};
 
-    use super::common::aio::{connect, create_vertex_with_label, drop_vertices};
+    use serial_test::serial;
+    
+    use gremlin_client::{aio::GremlinClient, process::traversal::traversal};
+
+    use super::common::aio::{connect_serializer, create_vertex_with_label, drop_vertices};
 
     #[cfg(feature = "async-std-runtime")]
     use async_std::prelude::*;
@@ -12,12 +17,17 @@ mod aio {
     #[cfg(feature = "tokio-runtime")]
     use tokio_stream::StreamExt;
 
-    use gremlin_client::Vertex;
+    use gremlin_client::{Vertex, IoProtocol};
 
+    #[rstest]
+    #[case::graphson_v2(connect_serializer(IoProtocol::GraphSONV2))]
+    #[case::graphson_v3(connect_serializer(IoProtocol::GraphSONV3))]
+    #[case::graph_binary_v1(connect_serializer(IoProtocol::GraphBinaryV1))]
+    #[awt]
     #[cfg_attr(feature = "async-std-runtime", async_std::test)]
     #[cfg_attr(feature = "tokio-runtime", tokio::test)]
-    async fn test_simple_vertex_traversal_with_multiple_id() {
-        let client = connect().await;
+    #[serial(test_simple_vertex_traversal_with_multiple_id)]
+    async fn test_simple_vertex_traversal_with_multiple_id(#[future] #[case] client: GremlinClient) {
         drop_vertices(&client, "test_simple_vertex_traversal_async")
             .await
             .unwrap();
